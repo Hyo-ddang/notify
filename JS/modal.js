46d240008f2cf6f8dc02d9b7ca7e7046beefc75b
Hyoddang <yhd5689@naver.com>
2024-08-25 21:14:47 +0900

    localStorage 추가

--- JS/modal.js
+++ JS/modal.js
@@ -1,9 +1,16 @@
+/*
+1. 추가 함수 - O
+2. 삭제 함수 -> 버튼 디테일 추가, 체크박스 주변 영역 클릭 시 선택되는 이벤트 추가
+3. 로컬스토리지 - O
+4. 수정 함수 -> value값을 받아 모달창에서 수정하기
+5. 게시물 클릭 시 모달화면 띄워서 보여주기
+*/
+
 const modalDisplay = document.querySelector(".modal-wrap");
 const modalOffAndSubmit = document.querySelector(".submit-btn");
 const modalOffCancel = document.querySelector(".cancel-btn");
 const modalOnAdd = document.querySelector(".add-btn");
 const modalRemoveBtn = document.querySelector(".remove-btn");
-
 
 const inputTitle = document.querySelector(".title");
 const inputContent = document.querySelector(".content");
@@ -83,3 +90,11 @@ modalOnAdd.addEventListener("click", modalDisplayOn);
 modalOffCancel.addEventListener("click", modalDisplayOff);
 modalOffAndSubmit.addEventListener("click", modalBtnHandler);
 modalRemoveBtn.addEventListener("click", notifyRemove);
+
+//* localStorage에 저장된 배열을 화면에 렌더링
+const savedNotifys = localStorage.getItem("notifys");
+if (savedNotifys !== null) {
+  const parsedNotifys = JSON.parse(savedNotifys);
+  notifys = parsedNotifys;
+  parsedNotifys.forEach(modalNotifyAdd);
+}
